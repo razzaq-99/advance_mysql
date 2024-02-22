1580428552a52a50ff7ae20ae5ff6cb50ea1e3b9
abdul Razzaq <ar8937562@gmail.com>
2024-02-22 17:06:58 +0500

    Default Storage Procedure, in and out parameters in procedure in mysql

--- advance_mysql.sql
+++ advance_mysql.sql
@@ -1,4 +1,4 @@
-                             -- views in mysql
+        -- views in mysql
 use classic_models;
 select * from classic_models.customers;
 
@@ -13,3 +13,68 @@ group by country order by count(customerNumber) asc;
 
 create view classic_models.price_of_products as 
 select * from classic_models.products where buyPrice > 50.50 order by buyPrice asc ;
+
+
+
+
+
+
+
+                                 -- Storage Procedure 
+
+Delimiter && 
+create procedure movies.get_data()
+begin 
+     select * from movies.flixpatrol;
+end && 
+Delimiter ;
+
+call movies.get_data();
+
+
+
+
+Delimiter && 
+create procedure classic_models.get_data_of_orderdetails()
+begin 
+     select * from classic_models.order_details;
+end && 
+Delimiter ;
+
+call classic_models.get_data_of_orderdetails();
+
+
+Delimiter && 
+create procedure movies.get_limit(in var int)
+begin 
+     select * from movies.flixpatrol limit var;
+end && 
+Delimiter ;
+
+call movies.get_data();
+call movies.get_limit(10);
+call movies.get_limit(20);
+
+
+
+Delimiter && 
+create procedure movies.get_data_out(out var int)
+begin 
+     select max(Watchtime) into var  from movies.flixpatrol;
+end && 
+Delimiter ;
+
+call movies.get_data_out(@HighestWatchTime);
+select @HighestWatchTime;
+
+
+
+Delimiter && 
+create procedure classic_models.get_result_out(out var int)
+begin 
+     select max(classic_models.products.buyPrice) into var  from classic_models.products;
+end && 
+Delimiter ;
+
+call classic_models.get_result_out(@buyprice);
+select @buyprice;
